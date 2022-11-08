b72ac2223a5367dd52f66bd936711c09960530f5
Noskov Vladimir <burger_ok@mail.ru>
2022-11-08 19:24:25 +0300

    add new game brain-calc

---
+++ games/calc.js
@@ -0,0 +1,31 @@
+import getRandom from '../src/getRandom.js';
+import playGame from '../src/index.js';
+
+const calculator = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return false;
+  }
+}
+const description = 'What is the result of the expression?';
+
+const getQuestionAndAnswerCorrect = () => {
+  const num1 = getRandom();
+  const num2 = getRandom();
+  const operators = ['+', '-', '*'];
+  const operator = operators[getRandom(0, 2)];
+
+  const question = `${num1} ${operator} ${num2}`;
+  const answerCorrect = String(calculator(num1, num2, operator));
+  return [question, answerCorrect];
+};
+
+const startGame = () => playGame(getQuestionAndAnswerCorrect, description);
+
+export default startGame;
