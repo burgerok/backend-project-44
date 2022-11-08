f6e8109ad318014afa164d034bb4a1809dff7434
Noskov Vladimir <burger_ok@mail.ru>
2022-11-09 00:05:35 +0300

    change "while" on "for" in src/index.js

--- src/index.js
+++ src/index.js
@@ -7,19 +7,18 @@ const playGame = (getQuestionAndAnswerCorrect, description) => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(description);
-  let i = 1;
-  while (i <= roundQuantity) {
+
+  for (let i = 0; i < roundQuantity; i += 1) {
     const [question, answerCorrect] = getQuestionAndAnswerCorrect();
     const answer = readlineSync.question(`Question: ${question}\nYour answer: `);
     if (answer === answerCorrect) {
       console.log('Correct!');
-      i += 1;
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${answerCorrect}'. \nLet's try again, ${userName}!`);
-      break;
+      return;
     }
-    if (i === 4) console.log(`Congratulations, ${userName}!`);
   }
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default playGame;
