a527fc4c97ff48b9c6b97d68757c612efdcf507c
Noskov Vladimir <burger_ok@mail.ru>
2022-11-12 15:59:57 +0300

    add new game brain-prime

---
+++ games/prime.js
@@ -0,0 +1,26 @@
+import getRandom from '../src/getRandom.js';
+import playGame from '../src/index.js';
+
+const prime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const getQuestionAndAnswerCorrect = () => {
+  const question = getRandom();
+  const answerCorrect = prime(question) ? 'yes' : 'no';
+  return [question, answerCorrect];
+};
+
+const startGame = () => playGame(getQuestionAndAnswerCorrect, description);
+
+export default startGame;
