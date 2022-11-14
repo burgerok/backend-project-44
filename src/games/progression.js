8a5fdd4f8c58eb47f7fbe08865d1569e61bcd55b
Noskov Vladimir <burger_ok@mail.ru>
2022-11-14 22:22:09 +0300

    consolidate code to generateQuestion function

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,24 +1,29 @@
 import getRandom from '../getRandom.js';
 import playGame from '../index.js';
 
+const generateQuestion = (start, length, step, index) => {
+  const progression = [];
+  progression[0] = start;
+  let question = String(progression[0]);
+  if (index === 0) question = '..';
+  for (let i = 1; i < length; i += 1) {
+    progression[i] = progression[i - 1] + step;
+    if (i === index) question = `${question} ..`;
+    else question = `${question} ${progression[i]}`;
+  }
+  return question;
+};
+
 const description = 'What number is missing in the progression?';
 
 const getQuestionAndAnswerCorrect = () => {
+  const progressionStart = getRandom(1, 10);
   const progressionLength = getRandom(5, 10);
   const progressionStep = getRandom(1, 10);
   const elementHiddenIndex = getRandom(0, progressionLength - 1);
-  const progression = [];
-  progression[0] = getRandom(1, 10);
-
-  let question = String(progression[0]);
-  if (elementHiddenIndex === 0) question = '..';
-
-  for (let i = 1; i < progressionLength; i += 1) {
-    progression[i] = progression[i - 1] + progressionStep;
-    if (i === elementHiddenIndex) question = `${question} ..`;
-    else question = `${question} ${progression[i]}`;
-  }
-  const answerCorrect = String(progression[elementHiddenIndex]);
+  
+  const question = generateQuestion(progressionStart, progressionLength, progressionStep, elementHiddenIndex);
+  const answerCorrect = String(progressionStart + progressionStep * elementHiddenIndex);
   return [question, answerCorrect];
 };
 
